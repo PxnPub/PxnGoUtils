13c2117c6acd9e1cf5b7929ffd52f5e8d8125f5e
lorenzop <lorenzo@poixson.com>
2025-05-30 23:39:29 -0400

    fixed san functions to support empty strings

--- san/san.go
+++ san/san.go
@@ -7,11 +7,11 @@ import(
 
 
 
-func IsSafeAlphaLower(str string) bool   { return Regexp.MustCompile("^[a-z]+$"        ).MatchString(str); }
-func IsSafeAlphaUpper(str string) bool   { return Regexp.MustCompile("^[A-Z]+$"        ).MatchString(str); }
-func IsSafeAlpha     (str string) bool   { return Regexp.MustCompile("^[a-zA-Z]+$"     ).MatchString(str); }
-func IsSafeAlphaNum  (str string) bool   { return Regexp.MustCompile("^[a-zA-Z0-9]+$"  ).MatchString(str); }
-func IsSafeFilePath  (str string) bool   { return Regexp.MustCompile("^[a-zA-Z0-9./]+$").MatchString(str); }
-func IsSafeDomain    (str string) bool   { return Regexp.MustCompile("^[a-zA-Z0-9.]+$" ).MatchString(str); }
-func IsSafeDomainPort(str string) bool   { return Regexp.MustCompile("^[a-zA-Z0-9.:]+$").MatchString(str); }
-func SafeHTML        (str string) string { return HTML.EscapeString(str);                                  }
+func IsSafeAlphaLower(str string) bool { if str == "" { return true; }; return Regexp.MustCompile("^[a-z]+$"        ).MatchString(str); }
+func IsSafeAlphaUpper(str string) bool { if str == "" { return true; }; return Regexp.MustCompile("^[A-Z]+$"        ).MatchString(str); }
+func IsSafeAlpha     (str string) bool { if str == "" { return true; }; return Regexp.MustCompile("^[a-zA-Z]+$"     ).MatchString(str); }
+func IsSafeAlphaNum  (str string) bool { if str == "" { return true; }; return Regexp.MustCompile("^[a-zA-Z0-9]+$"  ).MatchString(str); }
+func IsSafeFilePath  (str string) bool { if str == "" { return true; }; return Regexp.MustCompile("^[a-zA-Z0-9./]+$").MatchString(str); }
+func IsSafeDomain    (str string) bool { if str == "" { return true; }; return Regexp.MustCompile("^[a-zA-Z0-9.]+$" ).MatchString(str); }
+func IsSafeDomainPort(str string) bool { if str == "" { return true; }; return Regexp.MustCompile("^[a-zA-Z0-9.:]+$").MatchString(str); }
+func SafeHTML(str string) string { return HTML.EscapeString(str); }
